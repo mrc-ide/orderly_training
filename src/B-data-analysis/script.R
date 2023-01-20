24a214be61fbf0e9f42d49383eae11933cb1670f
kgaythorpe <k.gaythorpe@imperial.ac.uk>
2023-01-20 13:39:36 +0000

    adding data analysis

---
+++ src/B-data-analysis/script.R
@@ -0,0 +1,30 @@
+# data analysis goes here
+options(dplyr.summarise.inform = FALSE)
+
+df <- readRDS("cleaned_data.rds")
+
+# table of data summary stats
+df %>%
+  group_by(disease) %>%
+  summarise(min_cov = min(coverage),
+            max_cov = max(coverage),
+            mean_cov = mean(coverage)) %>%
+  saveRDS("summary_cov_stat.rds")
+
+#figure showing both datasets and trends
+p <- df %>%
+  mutate(disease = toupper(disease)) %>%
+  ggplot()+
+  aes(x = date,
+      y = coverage,
+      colour = disease, 
+      fill = disease)+
+  geom_point()+
+  geom_smooth()+
+  theme_minimal()+
+  scale_fill_met_d("Renoir")+
+  scale_color_met_d("Renoir")+
+  labs(x = "Date", y = "Coverage", fill = "Disease", colour = "Disease")+
+  ylim(0,1)
+
+ggsave(plot = p, filename = "coverage.png")
